a1de9b9d7fdcd11a0a79ea29e3f370396ed862e8
torstenfaruhn <torsten.faruhn@delimburger.nl>
2026-02-17 13:13:31 +0100

    Add files via upload

--- src/llm/promptBuilder.js
+++ src/llm/promptBuilder.js
@@ -7,6 +7,8 @@ function buildInstructions({stylebookText}){
     'Schrijf in B1, neutraal, zonder marketingtaal.',
     'Citaten blijven letterlijk en worden toegeschreven.',
     'Waarom en Hoe vul je alleen in als dat letterlijk in de bron staat.',
+    'Lengte: KOP maximaal 150 tekens (incl. spaties). INTRO + BODY samen tussen 1750 en 1950 tekens (incl. spaties).',
+    'Neem geen contactblok, noten voor de redactie of tekst die niet voor publicatie is over in kop/intro/body.',
     'Geef ALLEEN geldige JSON terug conform het schema. Geen extra tekst.'
   ].join('\n')+style;
 }
