77bd87f9782f30801a2a36d7b7d56e1d74a88f28
torstenfaruhn <torsten.faruhn@delimburger.nl>
2026-02-18 10:52:20 +0100

    Delete src directory

--- src/validators/messages.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-const M={E004:'Te weinig brontekst om een nieuwsbericht te genereren.',E006:'Brontekst voldoet niet aan minimumeisen: 5W\u2019s+H.',E007:'Meerdere persberichten gedetecteerd. Upload 1 persbericht per keer.',E005:'Maximale verwerkingstijd overschreden. Herstart de tool (Ctrl+F5) en probeer het opnieuw.'};
-module.exports={M};
--- src/validators/w007_lengthOutOfRange.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-function cc(s){return String(s||'').replace(/\r\n|\n|\r/g,' ').replace(/\s+/g,' ').trim().length;}
-function lengthWarnings({intro,body}){
-  const total=cc(intro)+cc(body);
-  if(total<=2150) return [];
-  return [{code:'W007',message:'Intro+body is langer dan 2150 tekens. Eindredacteur moet inkorten.'}];
-}
-module.exports={lengthWarnings};
