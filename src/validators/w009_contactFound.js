7b03342896094f0c8c61b66eeff74e2008bc9fdd
torstenfaruhn <torsten.faruhn@delimburger.nl>
2026-02-19 11:02:57 +0100

    Delete src directory

--- src/validators/w009_contactFound.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-function contactWarnings(){return [{code:'W009',message:'Contactinformatie gevonden. Controleer het contactblok (niet voor publicatie).'}];}
-module.exports={contactWarnings};
