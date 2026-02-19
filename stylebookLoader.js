9ed507c7e71687d16556c9d509f4afe538963083
torstenfaruhn <torsten.faruhn@delimburger.nl>
2026-02-19 11:59:41 +0100

    Delete stylebookLoader.js

--- stylebookLoader.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-function normalizeText(raw){
-  const oneLine=String(raw||'').replace(/\r\n|\n|\r/g,' ');
-  const single=oneLine.replace(/\s+/g,' ').trim();
-  return {text:single,charCount:single.length};
-}
-module.exports={normalizeText};
