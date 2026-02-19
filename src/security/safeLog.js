80e3efb529b0768f27fa6a772f2c16df0ec37e57
torstenfaruhn <torsten.faruhn@delimburger.nl>
2026-02-19 11:42:16 +0100

    Delete src directory

--- src/security/safeLog.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-function safeLog(message){
-  if(!message||typeof message!=='string')return;
-  const clean=message.replace(/[\r\n]/g,' ').slice(0,200);
-  console.log(clean);
-}
-module.exports={safeLog};
